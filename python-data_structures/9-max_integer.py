142acfe4346c9ccb583c9f96215fe97cf75af2d3
root <ryan-055@outlook.com>
2025-07-24 16:32:22 +0000

    task 10

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    num = my_list[0]
     if not my_list:
         return None
+    num = my_list[0]
     for i in my_list:
         if i > num:
             num = i
