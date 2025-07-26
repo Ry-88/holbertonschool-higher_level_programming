e83bb3dfeb41364027351038e8d04d892f46bd8b
root <ryan-055@outlook.com>
2025-07-26 23:10:12 +0000

    task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -8,17 +8,8 @@ casts them to integers, and returns their sum.
 
 def add_integer(a, b=98):
 
-    """Add two integers or floats (casted to integers).
-
-    Args:
-        a: The first number (int or float).
-        b: The second number (int or float), default is 98.
-
-    Returns:
-        int: The sum of a and b, casted to integers.
-
-    Raises:
-        TypeError: If a or b are not int or float.
+    """Add two integers or floats.
+    Casts floats to ints and returns the sum.
     """
     if (not isinstance(a, (int, float)) or
         (isinstance(a, float) and
