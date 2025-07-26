a9071503ab7138e1f65b4fb9e4b0c8c83f1d3d00
root <ryan-055@outlook.com>
2025-07-26 22:17:40 +0000

    fix pycodestyle

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,7 +5,9 @@ The function ensures the inputs are integers or floats,
 casts them to integers, and returns their sum.
 """
 
+
 def add_integer(a, b=98):
+
     """Add two integers or floats (casted to integers).
 
     Args:
@@ -18,9 +20,18 @@ def add_integer(a, b=98):
     Raises:
         TypeError: If a or b are not int or float.
     """
-    if not isinstance(a, (int, float)):
+    if not isinstance(a, (int, float)) or isinstance(a, float) and a != a:
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
+    if not isinstance(b, (int, float)) or isinstance(b, float) and b != b:
         raise TypeError("b must be an integer")
 
-    return int(a) + int(b)
+    try:
+        a_int = int(a)
+    except OverflowError:
+        raise TypeError("a must be an integer")
+    try:
+        b_int = int(b)
+    except OverflowError:
+        raise TypeError("b must be an integer")
+
+    return a_int + b_int
