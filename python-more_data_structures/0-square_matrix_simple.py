64d09f11e15bf693915928faf3b150a1c6a0e38a
root <ryan-055@outlook.com>
2025-07-26 14:57:58 +0000

    task 0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,7 @@
+#!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    # Create a new matrix using list comprehension
     new_matrix = []
     for row in matrix:
-        # Square each element in the row and create a new row
         new_row = [x ** 2 for x in row]
         new_matrix.append(new_row)
     return new_matrix
