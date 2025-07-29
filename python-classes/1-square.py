8c4947a543f1c3b8663d354b6093f8c55103a8d4
root <ryan-055@outlook.com>
2025-07-29 15:41:09 +0000

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+empty class Square that defines a square
+"""
+
+
+class Square:
+    """
+    defines a class Square
+    """
+
+    def __init__(self, size):
+        """
+        Initialize the square with a given size
+
+        Args:
+            size: The size of the square's
+        """
+
+        self.__size = size
