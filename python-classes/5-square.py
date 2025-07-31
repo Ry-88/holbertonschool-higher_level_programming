60b1b8e4c3a4d6f6307dd74266a6988965d81211
Rayan Hantoul <ryan-055@outlook.com>
2025-07-31 19:44:58 +0300

    task 5

---
+++ python-classes/5-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+class Square that defines a square
+"""
+
+
+class Square:
+    """
+    defines a class Square
+    """
+
+    def __init__(self, size=0):
+        """
+        Initialize the square with a given size
+
+        Args:
+            size: The size of the square's
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """access a method like it's a regular variable"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """acsses to __size and check
+        set a value to __size"""
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        Calculate and return the area of the square
+
+        Returns:
+            The area of the square
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """prints in stdout the square with the character #"""
+
+        if self.size == 0:
+            print()
+        else:
+            for i in range(self.size):
+                print("#" * self.size)
