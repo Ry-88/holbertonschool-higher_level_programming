15fe643bff54f3663bbff04560dc56e79bc81687
Rayan Hantoul <ryan-055@outlook.com>
2025-08-04 21:24:19 +0300

    fix code __str__

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -8,3 +8,7 @@ class MyList(list):
     def print_sorted(self):
         """prints the list on ascending sort"""
         print(sorted(self))
+
+    def __str__(self):
+        """Customize print behavior of your object"""
+        return super().__str__()
