2075a856512991b174b6bebb4f85abe1590af6db
Rayan Hantoul <ryan-055@outlook.com>
2025-08-05 19:33:52 +0300

    task 4

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""defain a function"""
+
+
+def inherits_from(obj, a_class):
+    """ a function that returns True if the object
+    is an instance of a class that inherited
+    (directly or indirectly) from the specified class ; otherwise False"""
+
+    if type(obj) is not a_class:
+        return True
+    else:
+        return False
