ceb1800a4f71c13d06e4a1ce740f1a6e4818a4f7
Rayan Hantoul <ryan-055@outlook.com>
2025-08-05 19:58:31 +0300

    task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""defain a class"""
+
+
+class BaseGeometry():
+    """defain a Public instance method"""
+
+    def area(self):
+        """raise an Exception"""
+
+        raise Exception("area() is not implemented")
