725beac85126724d6468f14a9716e1f604f46942
Rayan Hantoul <ryan-055@outlook.com>
2025-08-06 19:33:12 +0300

    task 10

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""a class Square that inherits from Rectangle"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """defain a class and inherit"""
+
+    def __init__(self, size):
+        """Instantiation size"""
+
+        self.__size = size
+        super().integer_validator("size", size)
+
+    def area(self):
+        """return a value of full rectangle"""
+
+        return self.__size * self.__size
+
+    def __str__(self):
+        """print description"""
+
+        return (f"[Rectangle] {self.__size}/{self.__size}")
