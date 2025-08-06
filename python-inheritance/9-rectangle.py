d874983bffe2f86e0db4c0f83a4030e6a1ccb220
Rayan Hantoul <ryan-055@outlook.com>
2025-08-06 19:08:45 +0300

    task 9

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""a class Rectangle that inherits from BaseGeometry"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """defain a class"""
+
+    def __init__(self, width, height):
+        """Instantiation width and height"""
+
+        self.__width = width
+        self.__height = height
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+
+    def area(self):
+        """return a value of full rectangle"""
+
+        return self.__width * self.__height
+
+    def __str__(self):
+        """print description"""
+
+        return (f"[Rectangle] {self.__width}/{self.__height}")
