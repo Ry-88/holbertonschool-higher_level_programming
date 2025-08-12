2de7174c70b972e17ed56a18527e704b1ec0a4c6
Rayan Hantoul <ryan-055@outlook.com>
2025-08-12 21:00:38 +0300

    fix code

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,6 +5,6 @@
 def read_file(filename=""):
     """function to read a file"""
 
-    with open("my_file_0.txt", "r", encoding="utf-8") as file:
-        for line in file:
-            print(line, end="")
+    with open(filename, "r", encoding="utf-8") as file:
+        content = file.read()
+        print(content, end="")
