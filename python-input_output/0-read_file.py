653f24069998c002abfe0ea6755fe5adc30d6ebe
Rayan Hantoul <ryan-055@outlook.com>
2025-08-12 20:44:07 +0300

    task 0

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""open and read the content of tile"""
+
+
+def read_file(filename=""):
+    """function to read a file"""
+
+    file = open("my_file_0.txt", "r")
+    content = file.read()
+    print(content)
+    file.close
