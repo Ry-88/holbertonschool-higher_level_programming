7d0aa51d4f5cfbd2134298acefb750cbc991805d
Rayan Hantoul <ryan-055@outlook.com>
2025-08-16 00:15:28 +0300

    fix pycodestyle

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -4,15 +4,16 @@ import json
 
 
 def serialize_and_save_to_file(data, filename):
-     """
+    """
     Serializes Python object to JSON and saves it into a file.
     """
-     with open(filename, "w", encoding="utf-8") as file:
-          json.dump(data, file)
+    with open(filename, "w", encoding="utf-8") as file:
+        json.dump(data, file)
+
 
 def load_and_deserialize(filename):
     """
     Loads JSON from file and deserializes it back to a Python object.
     """
     with open(filename, "r", encoding="utf-8") as file:
-         return json.load(file)
+        return json.load(file)
