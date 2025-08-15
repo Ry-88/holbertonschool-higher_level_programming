be0c7b6b5a9f25113ee67fc1dc147df7202dc8fb
Rayan Hantoul <ryan-055@outlook.com>
2025-08-15 19:16:55 +0300

    fix pycodestyle

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -7,6 +7,7 @@ for JSON serialization of an object"""
 def class_to_json(obj):
     """
     Returns the dictionary description with simple data structure
-    (list, dictionary, string, integer and boolean) for JSON serialization of an object.
+    (list, dictionary, string, integer and boolean)
+    for JSON serialization of an object.
     """
     return obj.__dict__
