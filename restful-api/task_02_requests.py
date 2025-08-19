1581b835a3130d4e1ffb6ec7dd313a9d18d2afd7
Rayan Hantoul <ryan-055@outlook.com>
2025-08-19 21:35:51 +0300

    fix code

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -6,7 +6,7 @@ import csv
 
 def fetch_and_print_posts():
     status = requests.get('https://jsonplaceholder.typicode.com/posts')
-    print("Status Code:", status.status_code)
+    print(f"Status Code: {status.status_code}")
     if status.status_code == 200:
         posts = status.json()
         for post in posts:
