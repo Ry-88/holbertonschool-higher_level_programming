f4d1b4b7b1f39ee0a59c26c08cbdfbfd357dc340
Rayan Hantoul <ryan-055@outlook.com>
2025-08-19 21:18:30 +0300

    fix code

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
 import requests
-import json
 
 
 def fetch_and_print_posts():
-    status = requests.get('https://api.github.com/user', auth=('user', 'pass'))
+    status = requests.get('https://jsonplaceholder.typicode.com/posts')
     print("Status Code:", status.status_code)
     if status.status_code == 200:
-        status.json()
-    print(status.headers['posts'])
+        posts = status.json()
+        for post in posts:
+            print(post["tilte"])
