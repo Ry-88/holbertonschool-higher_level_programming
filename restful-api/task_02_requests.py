11e10c5acd23a5068f86e70854c842f5318bf3bc
Rayan Hantoul <ryan-055@outlook.com>
2025-08-19 21:33:49 +0300

    task 2

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import requests
+import csv
 
 
 def fetch_and_print_posts():
@@ -10,3 +11,17 @@ def fetch_and_print_posts():
         posts = status.json()
         for post in posts:
             print(post["tilte"])
+
+def fetch_and_save_posts():
+    status = requests.get('https://jsonplaceholder.typicode.com/posts')
+    if status.status_code == 200:
+        posts = status.json()
+        
+        posts_dec = [
+            {"id": post["id"], "title": post["title"], "body": post["body"]}
+            for post in posts
+        ]
+        with open("posts.csv", "w", encoding="utf-8") as file:
+            w = csv.DictWriter(file, fieldnames=["id", "title", "body"])
+            w.writeheader()
+            w.writerow(posts_dec)
