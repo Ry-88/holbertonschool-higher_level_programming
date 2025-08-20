09653593064b7308813cf68acbdcd46d0d47a6ae
Rayan Hantoul <ryan-055@outlook.com>
2025-08-20 21:44:40 +0300

    fix error

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -30,7 +30,7 @@ class SimpleRequest(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
-            self.wfile.write(b"Ok")
+            self.wfile.write(b"OK")
         else:
             self.send_response(404)
             self.send_header("Content-type", "text/plain")
