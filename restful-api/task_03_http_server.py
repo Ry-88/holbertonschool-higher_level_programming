c0c5be1d81fee5838d8163258ce9bd978da7ca95
Rayan Hantoul <ryan-055@outlook.com>
2025-08-20 21:34:18 +0300

    fix code

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -5,19 +5,19 @@ import json
 class SimpleRequest(BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == "/":
-            self.send_response(200, "Ok")
+            self.send_response(200)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
             self.wfile.write(b"Hello, this is a simple API!")
         elif self.path == "/data":
-            self.send_response(200, "Ok")
+            self.send_response(200)
             self.send_header("Content-type", "application/json")
             self.end_headers()
 
             data = {"name": "John", "age": 30, "city": "New York"}
             self.wfile.write(json.dumps(data).encode("utf-8"))
         elif self.path == "/info":
-            self.send_response(200, "Ok")
+            self.send_response(200)
             self.send_header("Content-type", "application/json")
             self.end_headers()
 
