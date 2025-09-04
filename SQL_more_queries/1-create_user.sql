6b1459acb2b566ea3c9dead9a9908c80d4b0f46b
root <ryan-055@outlook.com>
2025-09-04 16:59:35 +0000

    task 1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,8 +1,4 @@
 -- create user with all privileges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
--- Grant all privileges
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-
--- Apply changes
 FLUSH PRIVILEGES;
