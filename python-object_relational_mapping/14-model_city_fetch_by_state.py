72c83c531f29deaecca9fe8732bf4e644065230e
root <ryan-055@outlook.com>
2025-09-06 20:00:30 +0000

    task 14

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""Module joining 2 tables: cities and states"""
+"""
+Prints all City objects from the database hbtn_0e_14_usa
+"""
 
-from sys import argv
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from model_state import Base, State
@@ -9,23 +11,31 @@ from model_city import City
 
 
 if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: ./14-model_city_fetch_by_state.py <username> "
+              "<password> <database>")
+        sys.exit(1)
+
+    username, password, db_name = sys.argv[1:4]
+
+    # Create engine and session
     engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost/{}".format(
-            argv[1], argv[2], argv[3]
-        ),
+        f"mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}",
         pool_pre_ping=True,
     )
     Base.metadata.create_all(engine)
 
     session = Session(engine)
-    query = (
+
+    # Query cities joined with states
+    cities = (
         session.query(City, State)
-        .join(State)
+        .join(State, City.state_id == State.id)
         .order_by(City.id.asc())
         .all()
     )
 
-    for city, state in query:
+    for city, state in cities:
         print(f"{state.name}: ({city.id}) {city.name}")
 
     session.close()
--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
-"""Module creating a City class"""
+"""
+City model definition
+"""
+
 from sqlalchemy import Column, Integer, String, ForeignKey
 from model_state import Base
 
 
 class City(Base):
-    """City class"""
-    __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, nullable=False)
+    """
+    City class mapped to the cities table
+    """
+    __tablename__ = "cities"
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
