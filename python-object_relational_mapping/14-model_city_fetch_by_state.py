e31caee3c44a89b4bee3ca7d7ae3c1909997bed2
root <ryan-055@outlook.com>
2025-09-06 19:58:37 +0000

    task 14

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,20 +1,31 @@
 #!/usr/bin/python3
-"""Module joining 2 tables"""
-from model_state import Base, State
-from model_city import City
+"""Module joining 2 tables: cities and states"""
+
+from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
-from sys import argv
+from model_state import Base, State
+from model_city import City
+
 
 if __name__ == "__main__":
     engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost/{}".format(argv[1], argv[2], argv[3]),
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            argv[1], argv[2], argv[3]
+        ),
         pool_pre_ping=True,
     )
     Base.metadata.create_all(engine)
 
     session = Session(engine)
-    for city, state in session.query(
-            City, State).join(State).order_by(City.id.asc()).all():
-        print(f'{state.name}: ({city.id}) {city.name}')
+    query = (
+        session.query(City, State)
+        .join(State)
+        .order_by(City.id.asc())
+        .all()
+    )
+
+    for city, state in query:
+        print(f"{state.name}: ({city.id}) {city.name}")
+
     session.close()
