44a2bfa3724a451360bc2a9d4a25530f69be26e0
root <ryan-055@outlook.com>
2025-09-06 19:39:11 +0000

    task 8

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -8,6 +8,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
+
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: ./script.py <username> <password> <database>")
@@ -16,7 +17,9 @@ if __name__ == "__main__":
     username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
 
     # Connect to the MySQL database
-    engine = create_engine(f"mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}")
+    engine = create_engine(
+        f"mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}"
+    )
     Session = sessionmaker(bind=engine)
     session = Session()
 
