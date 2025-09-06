8693408840ad74324880587606ee6daa139fef0c
root <ryan-055@outlook.com>
2025-09-06 18:35:56 +0000

    task 4

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -26,10 +26,12 @@ if __name__ == "__main__":
     cursor = db.cursor()
 
     # Execute a single SQL query
-    cursor.execute("SELECT cities.id, cities.name, states.name "
+    cursor.execute(
+        "SELECT cities.id, cities.name, states.name "
         "FROM cities "
         "JOIN states ON cities.state_id = states.id "
-        "ORDER BY cities.id ASC")
+        "ORDER BY cities.id ASC"
+    )
 
     # Fetch and print results
     cities = cursor.fetchall()
