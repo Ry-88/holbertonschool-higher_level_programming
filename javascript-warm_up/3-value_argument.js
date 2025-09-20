8ea84a8b67daeba0715c372efac7263d0b2a8ef1
root <ryan-055@outlook.com>
2025-09-20 19:44:48 +0000

    fix

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
+
+// Get the first argument passed to the script
 const arg = process.argv[2];
+
+// Check if the argument exists
 if (arg === undefined) {
   console.log('No argument');
 } else {
