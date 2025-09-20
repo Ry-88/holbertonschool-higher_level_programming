5d911fd7caa1d43379797c509fd6d6e16c8d1461
root <ryan-055@outlook.com>
2025-09-20 19:55:19 +0000

    task 6

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -4,17 +4,16 @@ const lines = [
     'C is fun',
     'Python is cool',
     'JavaScript is amazing'
-  ];
+  ]
   
-  let output = '';
-  let i = 0;
+  let output = ''
+  let i = 0
   
   while (i < lines.length) {
-    output += lines[i];
-    i++;
-    // Only add newline if not the last element
-    output += i < lines.length ? '\n' : '';
+    output += lines[i]
+    i++
+    output += i < lines.length ? '\n' : ''
   }
   
-  console.log(output);
+  console.log(output)
   
