8adbe2bd54a95473e3bcc34462a042f7e59e7de7
root <ryan-055@outlook.com>
2025-09-20 20:03:00 +0000

    task 8

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,20 +1,12 @@
 #!/usr/bin/node
 
-const arg = process.argv[2];
-const size = parseInt(arg, 10);
+const arg = process.argv[2]
+const size = parseInt(arg, 10)
 
 if (isNaN(size)) {
-  console.log('Missing size');
+  console.log('Missing size')
 } else {
-  let i = 0;
-  while (i < size) {
-    let line = '';
-    let j = 0;
-    while (j < size) {
-      line += 'X';
-      j++;
-    }
-    console.log(line);
-    i++;
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size))
   }
 }
