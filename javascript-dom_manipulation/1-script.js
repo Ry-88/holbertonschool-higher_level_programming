e26cd0ac4d3c0184bd4f2b6c4f7954b982d1e684
Rayan Hantoul <ryan-055@outlook.com>
2025-09-27 02:18:30 +0300

    task 1

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,4 @@
+// Add click event listener to the element with id 'red_header'
+document.querySelector('#red_header').addEventListener('click', function () {
+  document.querySelector('header').style.color = '#FF0000';
+});
