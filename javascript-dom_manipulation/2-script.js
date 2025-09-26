05e1325a9efa452610fb1c07e9d3024fdf806b85
Rayan Hantoul <ryan-055@outlook.com>
2025-09-27 02:21:43 +0300

    task 2

---
+++ javascript-dom_manipulation/2-script.js
@@ -0,0 +1,4 @@
+// Add click event listener to #red_header
+document.querySelector('#red_header').addEventListener('click', function () {
+  document.querySelector('header').classList.add('red');
+});
