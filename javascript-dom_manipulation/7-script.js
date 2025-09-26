1eabcd75e8f628e4f4b1ed556cce194600d96c7e
Rayan Hantoul <ryan-055@outlook.com>
2025-09-27 02:46:13 +0300

    task 7

---
+++ javascript-dom_manipulation/7-script.js
@@ -0,0 +1,17 @@
+// Fetch list of movies from SWAPI
+fetch('https://swapi-api.hbtn.io/api/films/?format=json')
+  .then(response => response.json())
+  .then(data => {
+    const movies = data.results;
+    const list = document.querySelector('#list_movies');
+
+    movies.forEach(movie => {
+      const li = document.createElement('li');
+      li.textContent = movie.title;
+      list.appendChild(li);
+    });
+  })
+  .catch(error => {
+    console.error('Error fetching movies:', error);
+    document.querySelector('#list_movies').innerHTML = '<li>Error loading movies</li>';
+  });
